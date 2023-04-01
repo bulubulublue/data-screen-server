90c71ba111defadf22cb70a7daef06509c161fe8
ChenCheng-0715 <bendelayoc@gmail.com>
2023-04-01 15:56:32 +0800

    feat: 实时修改数据

--- service/server.js
+++ service/server.js
@@ -4,6 +4,7 @@ var http = require('http')
 const path = require('path')
 const fileReader = require('../utils/index.js').getFileJsonData
 const filePath = path.join(__dirname, '../data/salesData.json')
+const dayjs = require('dayjs')
 
 var httpServer = http.createServer().listen(8080, () => {
   console.log('create server success')
@@ -24,12 +25,23 @@ module.exports.listen = () => {
     connection.on('message', async (msg) => {
       console.log(msg) //{ type: 'utf8', utf8Data: '{"functionName":"test"}' }
       const payload = JSON.parse(msg.utf8Data)
-      // const data = await fileReader(filePath)
+      const data = JSON.parse(await fileReader(filePath))
       // payload.data = data
-      payload.data = { data: 'test' }
+      payload.data = data
+      connection.send(JSON.stringify(payload))
+      let date = dayjs(1650137397)
       // console.log(payload)
       // // 推送数据给客户端
-      connection.send(JSON.stringify(payload)) // 传递对象需要使用JSON.stringify
+      setInterval(() => {
+        date = date.add(10, 'minute')
+        // console.log(payload)
+        payload.data.totalOrders += 10
+        payload.data.date.push(date.format('HH:mm'))
+        payload.data.date.splice(0, payload.data.date.length - 15)
+        payload.data.data.push(Math.floor(Math.random() * 1000) + 1000)
+        payload.data.data.splice(0, payload.data.data.length - 15)
+        connection.send(JSON.stringify(payload)) // 传递对象需要使用JSON.stringify
+      }, 1000)
     })
   })
 }
